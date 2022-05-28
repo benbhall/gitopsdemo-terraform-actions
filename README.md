4b69c8e599e6d4f5151616a10d68db69acf61394
Ben Hall <3179852+benbhall@users.noreply.github.com>
2022-05-28 11:39:28 +0100

    Initial files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Example of Terraform + Azure + GitHub Actions
---
+++ main.tf
@@ -0,0 +1,68 @@
+locals {
+  location = "uksouth"
+  prefix   = "gitopsdemo"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "gitopsdemo-tfstates-rg"
+    storage_account_name = "gitopsdemostore"
+    container_name       = "gitopsdemotfstates"
+    key                  = "gitopsdemo.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${local.prefix}-rg"
+  location = local.location
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "${local.prefix}storageacct"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_application_insights" "main" {
+  name                = "${local.prefix}-appinsights"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  application_type    = "web"
+}
+
+resource "azurerm_service_plan" "main" {
+  name                = "${local.prefix}-asp"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "main" {
+  name                = "${local.prefix}-function"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  service_plan_id            = azurerm_service_plan.main.id
+  storage_account_name       = azurerm_storage_account.main.name
+  storage_account_access_key = azurerm_storage_account.main.primary_access_key
+
+  site_config {}
+
+  app_settings = {
+    AppInsights_InstrumentationKey = azurerm_application_insights.main.instrumentation_key
+  }
+}
